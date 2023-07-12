91acf06efb2bd1dc29391dbf6c159b13f7915dfc
iamcolekim <76983801+iamcolekim@users.noreply.github.com>
2023-07-12 18:31:56 -0400

    update about.js + adjusted styling

--- src/components/pages/landing/info/about.js
+++ src/components/pages/landing/info/about.js
@@ -4,11 +4,13 @@ const About = () => {
     return (
         <>
             <section id="projInfo" className="text-themeSilver bg-themeNavy">
-                <h1>
-                    We are RA<sup>2</sup>D: Robotics-Assisted Accelerated Discovery
-                </h1>
-                <h2>Our Mission</h2>
-                <p>Science discoveries take time and effort. We introduce novel robotic & AI-driven solutions to accelerate these discoveries.</p>
+                <div className="xl:mx-[100px] sm:mx-[50px]">
+                    <h1>
+                        We are RA<sup>2</sup>D: Robotics-Assisted Accelerated Discovery
+                    </h1>
+                    <h2>Our Mission</h2>
+                    <p>Science discoveries take time and effort. We introduce novel robotic & AI-driven solutions to accelerate these discoveries.</p>
+                </div>
             </section>
         </>
     );
